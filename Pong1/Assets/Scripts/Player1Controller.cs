a0cafef4e01fe36d80ae46c88442aadaa1de260c
Behzad <behzad120048@gmail.com>
2020-01-27 00:41:49 -0500

    Player 1 & 2 move separately
    Added additional axis for player2 controls and removed the alt axis for player1

---
+++ Pong1/Assets/Scripts/Player1Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controller : MonoBehaviour
+{
+    private float speed = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horiz = Input.GetAxis("HorizontalWASD");
+        float vert = Input.GetAxis("VerticalWASD");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horiz* speed, vert * speed);
+    }
+}
---
+++ Pong1/Assets/Scripts/Player2Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Controller : MonoBehaviour
+{
+    private int speed = 6;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vert = Input.GetAxis("VerticalArrows");
+        float horiz = Input.GetAxis("HorizontalArrows");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(horiz * speed, vert * speed);
+    }
+}
